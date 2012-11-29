8f916691115e9f9c01aadb66669317d6828a5934
Herpiko Dwi Aguno <herpiko@gmail.com>
2012-11-29 13:35:54 +0800

    tambah autorefresh di inbox

--- main/inbox.php
+++ main/inbox.php
@@ -8,5 +8,25 @@ menu($page);
 <?php
 include "../view/footer.php";
 ?>
+<!--muat ulang otomatis setiap 10 detik-->
+<script language="JavaScript" type="text/javascript">  
+var count =10
+var redirect="../main/inbox.php"  
+function countDown(){  
+ if (count <=1){  
+  window.location = redirect;  
+ }else{  
+  count--;  
+  setTimeout("countDown()", 1000)  
+ }  
+}  
+</script>   
+<span id="timer">  
+<script>  
+ countDown();  
+</script>  
+</span>
+<br><br>
+
 </body>
 </html>
